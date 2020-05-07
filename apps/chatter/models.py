8f76455eb871f3bbc85aa6ee2b91fbf74a9b28be
bkmaibach <keith@inputlogic.ca>
2020-05-07 10:16:16 -0700

    Add models, permissions, views, and serializers

--- apps/chatter/models.py
+++ apps/chatter/models.py
@@ -1,7 +1,29 @@
 from django.db import models
+from django.utils import timezone
+from django.conf import settings
 
 class Room(models.Model):
     name = models.CharField(max_length=80)
 
     def __str__(self):
         return self.name
+
+class Message(models.Model):
+    text = models.CharField(max_length=255)
+    timestamp = models.DateTimeField(auto_now_add=True)
+    user_profile = models.ForeignKey(
+        settings.AUTH_USER_MODEL,
+        on_delete=models.DO_NOTHING
+    )
+
+    room = models.ForeignKey(
+        Room,
+        on_delete=models.DO_NOTHING
+    )
+
+    def was_published_recently(self):
+        return timezone.now() - datetime.timedelta(days=1) <= self.timstamp <= timezone.now()
+    
+    def __str__(self):
+        """Return the model as a string"""
+        return self.timestamp + " - " + self.text
--- apps/chatter/serializers.py
+++ apps/chatter/serializers.py
@@ -1,7 +1,13 @@
 from rest_framework import serializers
-from .models import Room
+from .models import Room, Message
 
 class RoomSerializer(serializers.HyperlinkedModelSerializer):
     class Meta:
         model = Room
         fields = ['name', 'id']
+
+class MessageSerializer(serializers.HyperlinkedModelSerializer):
+    class Meta:
+        model = Message
+        fields = ('id', 'timestamp', 'user_profile', 'text')
+        extra_kwargs = {'user_profile': {'read_only': True}}
