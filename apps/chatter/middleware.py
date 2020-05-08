cd3aca4752959d69ae724315c2a701b69b59285a
bkmaibach <keith@inputlogic.ca>
2020-05-08 09:32:09 -0700

    Prepare branch for PR

--- apps/chatter/middleware.py
+++ apps/chatter/middleware.py
@@ -8,6 +8,7 @@ from rest_framework.authtoken.models import Token
 # To attempt to activate this dumpster fire:
 # put TokenAuthMiddlewareStack in place of AuthMiddlewareStack
 # In project level routing.py
+# Currently gives error that scope['user'] is NoneType
 @database_sync_to_async
 def get_user(query_string):
     try:
